903b8b8eeb8c6f80ef3317bf40972ce35db970aa
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2020-01-07 15:20:16 +0100

    Dodano ograniczenia stawiania Barszczu Sosnowskiego przez uznanie go jako rozszrzenie klasy BushBlock

--- src/main/java/com/zsp/cfg/barszczsosnowski/blocks/Barszcz.java
+++ src/main/java/com/zsp/cfg/barszczsosnowski/blocks/Barszcz.java
@@ -1,11 +1,11 @@
 package com.zsp.cfg.barszczsosnowski.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BushBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
 
-public class Barszcz extends Block {
+public class Barszcz extends BushBlock {
 
     public Barszcz() {
         super(Properties.create(Material.CORAL)
