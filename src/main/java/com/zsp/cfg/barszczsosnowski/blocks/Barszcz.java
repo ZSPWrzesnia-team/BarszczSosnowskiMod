1a922b9d063670600aeb9fa8768a5f3b9c012d35
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2020-01-14 16:19:38 +0100

    Rozpoczęto prace nad losowym rozprzestrzenianiem się barszczu sosnowskiego.
    Aktywowano losowe ticki i dodano debugowe print'y

--- src/main/java/com/zsp/cfg/barszczsosnowski/blocks/Barszcz.java
+++ src/main/java/com/zsp/cfg/barszczsosnowski/blocks/Barszcz.java
@@ -1,9 +1,15 @@
 package com.zsp.cfg.barszczsosnowski.blocks;
 
+import com.zsp.cfg.barszczsosnowski.setup.BarszczConfig;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.BushBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
+
+import java.util.Random;
 
 public class Barszcz extends BushBlock {
 
@@ -13,7 +19,18 @@ public class Barszcz extends BushBlock {
                 .hardnessAndResistance(1.0f)
                 .doesNotBlockMovement()
                 .harvestTool(ToolType.SHOVEL)
-                .noDrops());
+                .noDrops()
+                .tickRandomly());
         setRegistryName("barszcz");
+    }
+
+    @Override
+    public void tick(BlockState state, World world, BlockPos pos, Random random) {
+        System.out.println("Barszcz @ " + pos.toString() + " Tick'd");
+        if (BarszczConfig.BARSZCZ_SPREAD_CHANCE < 0 || BarszczConfig.BARSZCZ_SPREAD_CHANCE > 1)
+            throw new IllegalStateException("BARSZCZ_SPREAD_CHANCE config is invalid. Cannot continue.");
+        if (random.nextDouble() < BarszczConfig.BARSZCZ_SPREAD_CHANCE) {
+            System.out.println("Barszcz @ " + pos.toString() + " Spread'd");
+        }
     }
 }
