2a8a4b2125f92a8ad93619a85660ee1052ff780e
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2020-01-21 15:28:41 +0100

    Zmniejszono szansę na rozprzestrzenienie Barszczu Sosnowskiego

--- src/main/java/com/zsp/cfg/barszczsosnowski/setup/BarszczConfig.java
+++ src/main/java/com/zsp/cfg/barszczsosnowski/setup/BarszczConfig.java
@@ -2,5 +2,5 @@ package com.zsp.cfg.barszczsosnowski.setup;
 
 public class BarszczConfig {
     public static final double BARSZCZ_SPAWN_CHANCE = 0.2;
-    public static final double BARSZCZ_SPREAD_CHANCE = 0.2;
+    public static final double BARSZCZ_SPREAD_CHANCE = 0.005;
 }
