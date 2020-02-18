3e37b237934f62ab173c3028f172ee298c251064
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2020-02-18 16:36:26 +0100

    Barszcz teraz będzie podpalał graczy bez odpowiedniego ekwipunku.
    Jeżeli gracz nie posiada pełenego set'a zbroi skórzanej, barszcz będzie podpalał go na 2s za każdą brakującą część zbroi.
    Podzczas kolizji z barszczem, każda cześć ma 2% szans na zminejszcznie pozostałej ilości uzyć o 1 co każdy tick.
    
    DO NAPRAWIENIA: Desynchronizacja ilości użyć zbroi między serwerem, a klientem.

--- src/main/java/com/zsp/cfg/barszczsosnowski/blocks/Barszcz.java
+++ src/main/java/com/zsp/cfg/barszczsosnowski/blocks/Barszcz.java
@@ -5,11 +5,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BushBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -46,6 +52,28 @@ public class Barszcz extends BushBlock {
                 BlockPos sPos = validBlocks.get(random.ints(1, 0, validBlocks.size()).iterator().next());
                 world.setBlockState(sPos, getDefaultState());
             }
+        }
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
+        //TODO: Naprawić desynchronizacje ilości użyć
+        if (!entityIn.isInvulnerableTo(DamageSource.IN_FIRE)) {
+            Iterable<ItemStack> armor = entityIn.getArmorInventoryList();
+            int armorPieces = 4;
+            Item[] requiredArmor = new Item[]{Items.LEATHER_HELMET, Items.LEATHER_CHESTPLATE, Items.LEATHER_LEGGINGS, Items.LEATHER_BOOTS, Items.LEATHER_HORSE_ARMOR};
+            for (ItemStack itemStack : armor)
+                if (Arrays.asList(requiredArmor).contains(itemStack.getItem())) {
+                    armorPieces--;
+                    if (new Random().nextFloat() <= 0.02f) {
+                        itemStack.setDamage(itemStack.getDamage() + 1);
+                        if (itemStack.getDamage() > itemStack.getMaxDamage()) {
+                            itemStack.shrink(1);
+                            itemStack.setDamage(0);
+                        }
+                    }
+                }
+            entityIn.setFireTimer(Math.max(160 / 4 * Math.max(0, armorPieces), entityIn.getFireTimer()));
         }
     }
 }
